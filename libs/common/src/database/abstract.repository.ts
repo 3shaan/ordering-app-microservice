61df6db4129bee169ff1cf0ac066f0d9a8fe2759
3shaan <3shaan.dev@proton.me>
2025-10-28 23:58:40 +0600

    feat(database): database abstruc schema , model and repository making...

---
+++ libs/common/src/database/abstract.repository.ts
@@ -0,0 +1,38 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import { AbstractDocument } from './abstract.schema';
+import { Connection, FilterQuery, Model, SaveOptions, Types } from 'mongoose';
+
+export abstract class AbstractRepository<TDocument extends AbstractDocument> {
+  protected abstract readonly logger: Logger;
+
+  constructor(
+    protected readonly model: Model<TDocument>,
+    private readonly connection: Connection,
+  ) {}
+
+  async create(
+    document: Omit<TDocument, '_id'>,
+    options?: SaveOptions,
+  ): Promise<TDocument> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+    return (
+      await createdDocument.save(options)
+    ).toJSON() as unknown as TDocument;
+  }
+
+  // find one
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model.findOne(filterQuery, {}, { lean: true });
+
+    if (!document) {
+      this.logger.warn('Document not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document as unknown as TDocument;
+  }
+}
