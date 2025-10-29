e8e7d8f4de0bba5cfea29f035e423300d0f8163f
3shaan <3shaan.dev@proton.me>
2025-10-29 15:52:25 +0600

    feat(order):order create working...

--- apps/orders/src/orders.controller.ts
+++ apps/orders/src/orders.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateOrderRequest } from './dto/create-order.request';
 import { OrdersService } from './orders.service';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
-  @Get()
-  getHello(): string {
-    return this.ordersService.getHello();
+  @Post()
+  createOrder(@Body() request: CreateOrderRequest) {
+    return this.ordersService.create(request);
   }
 }
--- apps/orders/src/orders.module.ts
+++ apps/orders/src/orders.module.ts
@@ -14,7 +14,7 @@ import { Order, OrderSchema } from './schemas/order.schema';
       isGlobal: true,
       validationSchema: Joi.object({
         // NODE_ENV: Joi.string().valid('development', 'production').required(),
-        // PORT: Joi.number().default(3000),
+        PORT: Joi.number().default(3000),
         MONGODB_URI: Joi.string().uri().required(),
       }),
       envFilePath: './apps/orders/.env',
